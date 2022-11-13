9129182809070fa19cd880895d81812368f5e233
Ivan Andryukha <64382696+JackGhoster@users.noreply.github.com>
2022-11-13 17:37:44 +0400

    mc movement
    I added movement to the main character and also limited the ability to surpass the viewport. :anguished:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,16 +7,22 @@ public class PlayerController : MonoBehaviour
 
     private PlayerControls playerControls;
     public Animator animator;
+    
     private bool isGoingUp = false;
     private bool isGoingRight = false;
 
-    private Vector2 MoveVector = Vector2.zero;
+    private Vector2 MoveInputVector = Vector2.zero;
+    private Vector2  ScreenBordersVector = new Vector2(1.1f, 0.5f);
+
+    private float speedDivider = 100f;
+
 
     // Start is called before the first frame update
     private void Awake()
     {
         playerControls = new PlayerControls();
         animator = GetComponent<Animator>();
+
     }
 
     void Start()
@@ -31,45 +38,89 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Move(MoveInputVector);
         
     }
 
 
-    private void Move(InputAction.CallbackContext context)
+    private void InputContextReader(InputAction.CallbackContext context)
     {
+        //Assignment of a two axis value of player's input to a Vector2 variable
+        MoveInputVector = context.ReadValue<Vector2>();
+
+        AnimationSwitcher();
+    
         ////vertical movement bool assignment (can use it later)
-        //isGoingUp = .y >= 0.5f ? isGoingUp = true : isGoingUp = false;
+        //isGoingUp = context.ReadValue<Vector2>().y >= 0.5f ? isGoingUp = true : isGoingUp = false;
 
         ////horizontal movement bool assignment (can use it later)
         //isGoingRight = context.ReadValue<Vector2>().x >= 0.5f ? isGoingRight = true : isGoingRight = false;
+    }
 
-        //Assignment of a two axis value of players input to a 
-        MoveVector = context.ReadValue<Vector2>();
 
-        AnimationSwitcher();
+    protected void Move(Vector2 InputVector)
+    {
+        if (InputVector == Vector2.zero) return;
+
+        this.transform.Translate(InputVector / speedDivider);
+
+        OnScreenBorderEnter();
+    }
+
+
+    private void OnScreenBorderEnter()
+    {
+        //Really sorry for the dirty code I was really lazy with this one
+
+        //The Vector that's used later to return character back to the game area
+        Vector2 ReturnerVector = Vector2.zero;
+
+        //When trying to cross the upper border character is returning back down
+        if (transform.position.y > ScreenBordersVector.y)
+        {
+            ReturnerVector = new(0, -0.01f);
+            transform.Translate(ReturnerVector);
+        }
+        //When trying to cross the bottom border character is returning back up
+        else if (transform.position.y < -ScreenBordersVector.y)
+        {
+            ReturnerVector = new(0, 0.01f);
+            transform.Translate(ReturnerVector);
+        }
+
+
+        //When trying to cross the right border character is returning back to the left
+        if (transform.position.x > ScreenBordersVector.x)
+        {
+            ReturnerVector = new(-0.01f, 0);
+            transform.Translate(ReturnerVector);
+        }
+        //When trying to cross the left border character is returning back to the right
+        else if (transform.position.x < -ScreenBordersVector.x)
+        {
+            ReturnerVector = new(0.01f, 0);
+            transform.Translate(ReturnerVector);
+        }
     }
 
     private void AnimationSwitcher()
     {
-        animator.SetFloat("VerticalAxis", MoveVector.y);
-        animator.SetFloat("HorizontalAxis", MoveVector.x);
-        //didnt work as intended, it's too complicating to switch beetwen them with 2 bools
-        //animator.SetBool("isGoingUp", isGoingUp);
-        //animator.SetBool("isGoingRight", isGoingRight);
+        animator.SetFloat("VerticalAxis", MoveInputVector.y);
+        animator.SetFloat("HorizontalAxis", MoveInputVector.x);
     }
 
 
     private void OnEnable()
     {
-        playerControls.Ground.Move.performed += Move;
-        playerControls.Ground.Move.canceled += Move;
+        playerControls.Ground.Move.performed += InputContextReader;
+        playerControls.Ground.Move.canceled += InputContextReader;
         playerControls.Enable();
         
     }
 
     private void OnDisable()
     {
-        playerControls.Ground.Move.performed -= Move;
+        playerControls.Ground.Move.performed -= InputContextReader;
         playerControls.Disable();
     }
 
